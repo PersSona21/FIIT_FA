c92c84befaaba99668cb53d6a4a1ef44e5bb0fa2
PersSona21 <perssona219@gmail.com>
2026-08-28 14:29:50 +0300

    исправил SplayTree.cs теперь оно без ошибок (проблема была в том что контаинс кей не обновлял дерево)

--- TreeDataStructures/Implementations/Splay/SplayTree.cs
+++ TreeDataStructures/Implementations/Splay/SplayTree.cs
@@ -8,19 +8,70 @@ public class SplayTree<TKey, TValue> : BinarySearchTree<TKey, TValue>
     protected override BstNode<TKey, TValue> CreateNode(TKey key, TValue value)
         => new(key, value);
     
+    public override bool ContainsKey(TKey key) => TryGetValue(key, out _);
+    
     protected override void OnNodeAdded(BstNode<TKey, TValue> newNode)
     {
-        throw new NotImplementedException();
+        Splay(newNode);
     }
     
     protected override void OnNodeRemoved(BstNode<TKey, TValue>? parent, BstNode<TKey, TValue>? child)
     {
-        throw new NotImplementedException();
+        if (parent != null) Splay(parent);
     }
     
     public override bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
     {
-        throw new NotImplementedException();
+        var current = Root;
+        BstNode<TKey, TValue>? last = null;
+
+        while (current != null)
+        {
+            last = current;
+            int cmp = Comparer.Compare(key, current.Key);
+
+            if (cmp == 0)
+            {
+                value = current.Value;
+                Splay(current);
+                return true;
+            }
+            current = cmp < 0 ? current.Left : current.Right;
+        }
+        
+        if (last != null) Splay(last);
+
+        value = default;
+        return false;
+    }
+
+    private void Splay(BstNode<TKey, TValue> node)
+    {
+        while (node.Parent != null)
+        {
+            
+            BstNode<TKey, TValue> parent = node.Parent;
+            BstNode<TKey, TValue>? grandparent = parent.Parent;
+            
+            if (grandparent == null)
+            {
+                // Zig - один поворот
+                if (node.IsLeftChild) RotateRight(parent);
+                else RotateLeft(parent);
+            }
+            else if (node.IsLeftChild == parent.IsLeftChild)
+            {
+                // Zig-zig - двойной поворот
+                if (node.IsLeftChild) RotateDoubleRight(grandparent);
+                else RotateDoubleLeft(grandparent);
+            }
+            else
+            {
+                // Zig-zag - большой поворот
+                if (node.IsLeftChild) RotateBigLeft(grandparent);
+                else RotateBigRight(grandparent);
+            }
+        }
     }
     
 }
